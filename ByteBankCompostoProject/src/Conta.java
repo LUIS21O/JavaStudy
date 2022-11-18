1ff67aa6e43ad32cafeef319da22833de7f47ca4
LUIS210 <luissotavios@outlook.com>
2022-11-18 09:46:18 -0300

    Aulas 5 e 6

--- ByteBankCompostoProject/src/Conta.java
+++ ByteBankCompostoProject/src/Conta.java
@@ -1,15 +1,48 @@
 public class Conta {
 
-    double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
 
     public void deposita (double valor) {
 
         this.saldo = this.saldo + valor;
 
     }
+
 
 
     // boolean para definir se vai poder sacar ou se não vai
--- ByteBankCompostoProject/src/TestaBanco.java
+++ ByteBankCompostoProject/src/TestaBanco.java
@@ -5,14 +5,14 @@ public class TestaBanco {
     public static void main(String[] args) {
 
         Cliente cliente1 = new Cliente();
-        cliente1.nome = "Luis Otavio";
-        cliente1.profissao = "Analista de Sistemas";
-        cliente1.cpf = "111.222.333-44";
+        cliente1.setNome("Luis Otavio");
+        cliente1.setProfissao("Analista de Sistemas");
+        cliente1.setCpf("111.222.333-44");
 
         Conta contaLuis = new Conta();
         contaLuis.deposita(100);
 
-        contaLuis.titular = cliente1;
-        System.out.println(contaLuis.titular.nome);
+        contaLuis.setTitular(cliente1);
+        System.out.println(contaLuis.getTitular().getNome());
     }
 }
